5af30ab8ac272a7247a3ceccf1106e91544508c2
Miki-mora <1011348487@qq.com>
2020-01-06 23:20:47 +0800

    picture

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -43,15 +43,15 @@ export default {
         {title:"泰国菜",image:"/static/images/4.png"}
       ],
       top: [
-        {name:"菜谱1",address:"地址1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍1"},
-        {name:"菜谱2",address:"地址2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍2"},
-        {name:"菜谱3",address:"地址3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍3"},
-        {name:"菜谱4",address:"地址4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍4"}
+        {name:"糖醋里脊",address:"口味：酸甜",image:"/static/images/z.jpg",remark:"主要食材：里脊肉，淀粉"},
+        {name:"韩国炸鸡",address:"口味：甜辣",image:"/static/images/r.jpg",remark:"主要食材：鸡腿，甜辣酱"},
+        {name:"意大利面",address:"口味：紧实而有弹性",image:"/static/images/x.jpg",remark:"主要食材：西红柿、洋葱、黄油、罗勒、黑胡椒、盐、意大利面、火腿、青椒"},
+        {name:"冬阴功汤",address:"口味：酸酸辣辣香香甜甜，五味俱全",image:"/static/images/t.jpeg",remark:"主要食材：柠檬叶，香茅，辣椒，虾"}
       ],
       imgUrls: [
-        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+        '/static/images/c.jpg',
+        '/static/images/b.jpg',
+        '/static/images/a.jpg'
       ],
       indicatorDots: false,
       autoplay: false,
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -6,6 +6,11 @@
     </view>
     <open-data type="userNickName"></open-data>
     </view>
+    <i-panel title="~~~~~美味佳肴，我要推荐！！！！~~~~~">
+      <i-input :value="name" @change="changeName($event)" title="名称" autofocus placeholder="请输入菜名" maxlength="20" />
+      <i-input :value="reason" @change="changeReason($event)" title="推荐理由" placeholder="请输入推荐理由" maxlength="50" />
+    </i-panel>
+    <i-button @click="handleClick" type="warning" :ghost="true" shape="circle" size="small" >我要推荐</i-button>
   </div>
 </template>
 
@@ -13,10 +18,35 @@
 export default {
   data () {
     return {
+      name:"",
+      reason:""
     }
   },
 
   methods: {
+    changeName (event) {
+      this.name = event.mp.detail.detail.value
+    },
+    changeReason (event) {
+      this.reason = event.mp.detail.detail.value
+    },
+    
+    handleClick () {
+      if (this.name && this.reason ) {
+        wx.showToast({
+          title: '推荐了' + this.name,
+          icon: 'warning',
+          duration: 2000
+        })
+        // TODO:将推荐数据提交到云数据库
+      } else {
+        wx.showToast({
+          title: '信息不完整',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    }
   },
 
   created () {
@@ -45,5 +75,11 @@ export default {
   border-radius: 50%;
   border: 2px solid #fff;
   box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
+
+.tips {
+  padding: 20pt;
+  font-size: 10pt;
+  color:lightblue;
 }
 </style>
